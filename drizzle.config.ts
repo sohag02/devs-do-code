b532a9216dc8c43fa3d09f3536fca392c9898289
Sohag Jabed <sohagjabed10@gmail.com>
2024-12-28 22:26:55 +0530

    initialize drizzle orm

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import { type Config } from "drizzle-kit";
+
+export default {
+  schema: "./db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL as string,
+  },
+} satisfies Config;
