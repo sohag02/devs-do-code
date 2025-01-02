0b49a156a5091694c14cb50d5167333460ea9f9b
Sohag Jabed <sohagjabed10@gmail.com>
2025-01-02 12:26:55 +0530

    feat: add Smart Prompt to settings panel

--- app/actions/chat.ts
+++ app/actions/chat.ts
@@ -1,15 +1,12 @@
 "use server";
 import { deleteChatById, renameChatById } from "@/db/queries";
-// import { revalidatePath } from 'next/cache'
 
 export async function renameChat(chatId: string, newTitle: string) {
   await renameChatById({ id: chatId, newTitle });
-  //   revalidatePath('/playground')
   return { id: chatId, title: newTitle };
 }
 
 export async function deleteChat(chatId: string) {
   await deleteChatById({ id: chatId });
-  //   revalidatePath('/playground')
   return { id: chatId };
 }
