c032f12f38971722d59892757a4e4ac5194d7771
Sohag Jabed <sohagjabed10@gmail.com>
2025-02-07 18:52:00 +0530

    add robots.txt and update metadata for SEO

--- app/metadata.ts
+++ app/metadata.ts
@@ -5,5 +5,22 @@ export const metadata: Metadata = {
   description: "Access GPT-4, Claude, and Gemini Pro all in one place. Compare responses and choose the best AI for your needs.",
   other: {
     'google-site-verification': 'IOK9F6S0dTgS0VsNrmJ1U4-D4eefejq4T4Ek1szdTl8',
+    'keyword': 'devsdocode',
+    'author': 'devsdocode',
+    'Slurp': 'all',
+    'Googlebot-Image': 'all',
+    'Scooter': 'all',
+    'WEBCRAWLERS': 'ALL',
+    'revisit-after': '2 days',
+    'robots': 'ALL, index, follow',
+    'distribution': 'Global',
+    'rating': 'Safe For All',
+    'language': 'English',
+    'window-target': '_top',
+    'pics-label': 'for all ages',
+    'GOOGLEBOTS': 'All, FOLLOW',
+    'YAHOOBOTS': 'All, FOLLOW',
+    'MSNBOTS': 'All, FOLLOW',
+    'BINGBOTS': 'All, FOLLOW',
   }
 }
